3533f011d8daf77ee4222e02390d0a2978f2e340
Max Jacobson <mjacobson7@gmail.com>
2019-08-05 23:43:28 -0600

    Display contests entered on front end

--- client/src/components/Dashboard.vue
+++ client/src/components/Dashboard.vue
@@ -4,42 +4,22 @@
     <table>
       <thead>
         <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Job Title</th>
-          <th>Twitter</th>
+          <th>Date</th>
+          <th>Screen Name</th>
+          <th>Text</th>
+          <th>Favorited</th>
+          <th>Retweeted</th>
+          <th>Followed</th>
         </tr>
       </thead>
-      <tbody>
+      <tbody v-for="contest in contests" :key="contest">
         <tr>
-          <td data-column="First Name">James</td>
-          <td data-column="Last Name">Matman</td>
-          <td data-column="Job Title">Chief Sandwich Eater</td>
-          <td data-column="Twitter">@james</td>
-        </tr>
-        <tr>
-          <td data-column="First Name">Andor</td>
-          <td data-column="Last Name">Nagy</td>
-          <td data-column="Job Title">Designer</td>
-          <td data-column="Twitter">@andornagy</td>
-        </tr>
-        <tr>
-          <td data-column="First Name">Tamas</td>
-          <td data-column="Last Name">Biro</td>
-          <td data-column="Job Title">Game Tester</td>
-          <td data-column="Twitter">@tamas</td>
-        </tr>
-        <tr>
-          <td data-column="First Name">Zoli</td>
-          <td data-column="Last Name">Mastah</td>
-          <td data-column="Job Title">Developer</td>
-          <td data-column="Twitter">@zoli</td>
-        </tr>
-        <tr>
-          <td data-column="First Name">Szabi</td>
-          <td data-column="Last Name">Nagy</td>
-          <td data-column="Job Title">Chief Sandwich Eater</td>
-          <td data-column="Twitter">@szabi</td>
+          <td data-column="date">{{contest.date}}</td>
+          <td data-column="Last Name">@{{contest.screenName}}</td>
+          <td data-column="Text">{{contest.text}}</td>
+          <td data-column="Favorited">{{contest.favorited}}</td>
+          <td data-column="Retweeted">{{contest.retweeted}}</td>
+          <td data-column="Followed">{{contest.followed}}</td>
         </tr>
       </tbody>
     </table>
@@ -52,6 +32,16 @@ import Header from "./Header";
 export default {
   components: {
     Header
+  },
+  data() {
+    return {
+      contests: null
+    };
+  },
+  mounted() {
+    this.$http.get("/contests").then(data => {
+      this.contests = data.body;
+    });
   }
 };
 </script>
